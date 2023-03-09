d4657fad736955617142cb80111170e84a8aa9b7
andrey.tsybulski <andrew@ttro.com>
2023-03-09 16:55:38 +0100

    added tests

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,8 +1,20 @@
 import superagent from "superagent";
 
-describe("API testing. GET method", () => {
-  it("Test GET user 2", async () => {
-    const res = await superagent.get("https://catfact.ninja/fact");
+const baseUrl = "https://catfact.ninja";
+
+describe("API testing. GET methods", () => {
+  it("Returns a list of breeds", async () => {
+    const res = await superagent.get(baseUrl + "/breeds?limit=1");
     expect(res.status).toBe(200);
-});
+  });
+
+  it("Returns a random cat fact 150", async () => {
+    const res = await superagent.get(baseUrl + "/fact?max_lenght=150");
+    expect(res.status).toBe(200);
+  });
+
+  it("Get a list of cat facts limit 5", async () => {
+    const res = await superagent.get(baseUrl + "/facts?limit=5&max_length=1");
+    expect(res.status).toBe(200);
+  });
 });
