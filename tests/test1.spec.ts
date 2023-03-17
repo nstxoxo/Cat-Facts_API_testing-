6caefd0eec0d5b303efcb3adccd93d5a0945c468
Anastasia <anyway98@mail.ru>
2023-03-17 09:38:15 +0300

    git commit -m

--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,18 +1,16 @@
-import superagent, { lock } from "superagent";
+import superagent from "superagent";
 import { EXPECTED_OBJ_GET_BREEDS } from "../objects/breedsObj";
 import { EXPECTED_OBJ_GET_FACTS } from "../objects/factsObj";
 import { BASE_URL, BREEDS, FACT, FACTS } from "../constans/url";
 import { EXPECTED_OBJ_GET_RANDOM_FACTS } from "../objects/randomFact";
 
 describe("API testing. GET methods", () => {
-  //ok
   it("Returns a list of breeds", async () => {
     const res = await superagent.get(BASE_URL + BREEDS);
     expect(res.status).toBe(200);
     expect(res.body).toEqual(EXPECTED_OBJ_GET_BREEDS);
   });
 
-  // ok
   it("Returns a list of breeds page 1", async () => {
     const res = await superagent.get(BASE_URL + BREEDS).query({ page: 1 });
     expect(res.status).toBe(200);
@@ -20,32 +18,24 @@ describe("API testing. GET methods", () => {
   });
 
   it("Returns a list of breeds limit 1", async () => {
-    const res = await superagent.get(BASE_URL + BREEDS).query({ limit: 1 });
-
+    const res = await superagent.get(BASE_URL + BREEDS).query({ limit: 3 });
     let breeds = EXPECTED_OBJ_GET_BREEDS.data;
-    let randomFact = res.body;
-    let factArray: any = [];
-    let hhh = factArray.push(randomFact);
-
-    const filteredFacts = factArray.filter((array: any) =>
+    let randomBreeds = res.body.data;
+    const filteredBreeds = randomBreeds.filter((array: any) =>
       breeds.every(
         (breeds: any) =>
-          !(factArray.fact === array.fact && breeds.lenght === array.length)
+          !(
+            randomBreeds.breed === array.breed &&
+            breeds.country === array.country &&
+            breeds.origin === array.origin
+          )
       )
     );
-    function cleanSingleRecord(filteredFacts: any) {
-      filteredFacts = JSON.stringify(filteredFacts);
-      filteredFacts = filteredFacts.substring(1, filteredFacts.length - 1);
-      return JSON.parse(filteredFacts.trim());
-    }
-
-    let objectFact = cleanSingleRecord(filteredFacts);
-
     expect(res.status).toBe(200);
-    // expect(res.body). (EXPECTED_OBJ_GET_BREEDS);
+    expect(randomBreeds).toEqual(filteredBreeds);
   });
 
-  it.only("Returns a random cat fact", async () => {
+  it("Returns a random cat fact", async () => {
     const res = await superagent.get(BASE_URL + FACT);
 
     let facts = EXPECTED_OBJ_GET_RANDOM_FACTS.data;
@@ -75,20 +65,9 @@ describe("API testing. GET methods", () => {
   it("Returns a random cat fact max lenght 150", async () => {
     const res = await superagent
       .get(BASE_URL + FACT)
-      .query({ max_lenght: 150 });
-    let facts = EXPECTED_OBJ_GET_RANDOM_FACTS.data;
-    let randomFacts = res.body;
-    let iii = [randomFacts];
-    // const filteredFacts = iii.filter((array: any) =>
-    //   facts.every(
-    //     (facts: any) =>
-    //       !(iii.fact === array.fact && facts.lenght === array.length)
-    //   )
-    // );
+      .query({ max_length: 150 });
     expect(res.status).toBe(200);
-    console.log(res.body.length);
-    // expect(res.body.length).toBeLessThanOrEqual(150);
-    // expect(randomFacts).toEqual(filteredFacts);
+    expect(res.body.length).toBeLessThanOrEqual(150);
   });
 
   it("Get a list of cat facts", async () => {
